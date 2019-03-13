3629a377b5965a4212dbecc9add7ebd181289cca
run <zhou_runliang@outlook.com>
2019-03-13 14:56:54 +0800

    first commit

---
+++ controllers/edit.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"strconv"
+	"time"
+	"go-blog/models"
+)
+
+type EditController struct {
+	beego.Controller
+}
+
+func (this *EditController) Get() {
+	id, _ := strconv.Atoi(this.Ctx.Input.Params()[":id"])
+	this.Data["Post"] = models.GetBlog(id)
+	this.Layout = "layout.tpl"
+	this.TplName = "edit.tpl"
+}
+
+func (this *EditController) Post() {
+	inputs := this.Input()
+	var blog models.Blog
+	blog.Id, _ = strconv.Atoi(inputs.Get("id"))
+	blog.Title = inputs.Get("title")
+	blog.Content = inputs.Get("content")
+	blog.Created = time.Now()
+	models.SaveBlog(blog)
+	this.Ctx.Redirect(302, "/")
+}
